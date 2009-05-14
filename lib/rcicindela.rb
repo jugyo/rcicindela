af20684436e611703d77e4753a65ad43ea141837
jugyo <kkohno@gmail.com>
2009-05-14 12:12:17 +0900

    Implemented

---
+++ lib/rcicindela.rb
@@ -0,0 +1,38 @@
+require 'open-uri'
+
+class RCicindela
+  attr_reader :base_uri
+
+  def initialize(base_uri)
+    @base_uri = base_uri.sub(/\/*$/, '')
+  end
+
+  %w(
+    record/insert_pick
+    record/insert_rating
+    record/insert_tag
+    record/set_category
+    record/insert_uninterested
+    record/delete_pick
+    record/delete_rating
+    record/delete_tag
+    record/delete_uninterested
+    record/remove_category
+    recommend/for_item
+    recommend/for_user
+    recommend/similar_users
+  ).each do |api_method|
+    prefix, method = api_method.split('/')
+    define_method(method) do |params|
+      get(prefix, {:op => method}.merge(params))
+    end
+  end
+
+  private
+
+  def get(prefix, params)
+    url = base_uri + '/' + prefix + '?' + params.map{ |k, v| [k.to_s, v.to_s] }.sort.map{ |i| i.join('=') }.join('&')
+    open(url).read
+  end
+end
+
---
+++ spec/rcicindela_spec.rb
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe RCicindela do
+  it 'should create instance with base_uri' do
+    uri = 'http://localhost/cicindela'
+    c = RCicindela.new(uri)
+    c.base_uri.should == uri
+  end
+
+  it 'should create instance with base_uri end "/"' do
+    uri = 'http://localhost/cicindela/'
+    c = RCicindela.new(uri)
+    c.base_uri.should == 'http://localhost/cicindela'
+  end
+
+  it 'should create instance with base_uri end "///"' do
+    uri = 'http://localhost/cicindela///'
+    c = RCicindela.new(uri)
+    c.base_uri.should == 'http://localhost/cicindela'
+  end
+
+  describe 'Create instance with "http://localhost/cicindela" as base_uri' do
+    before(:each) do
+      @cicindela = RCicindela.new('http://localhost/cicindela')
+    end  
+
+    it 'call method "get" when call insert_pick' do
+      @cicindela.should_receive(:get).with('record', {:op => 'insert_pick', :set => 'test', :user_id => 1, :item_id => 1})
+      @cicindela.insert_pick(:set => 'test', :user_id => 1, :item_id => 1)
+    end
+
+    it 'call method "get" when call for_item' do
+      @cicindela.should_receive(:get).with('recommend', {:op => 'for_item', :set => 'test', :item_id => 1})
+      @cicindela.for_item(:set => 'test', :item_id => 1)
+    end
+
+    it 'call method "open" when call insert_pick' do
+      @cicindela.should_receive(:open).
+        with("http://localhost/cicindela/record?item_id=1&op=insert_pick&set=test&user_id=1").and_return(StringIO.new('result'))
+      result = @cicindela.insert_pick(:set => 'test', :user_id => 1, :item_id => 1)
+      result.should == 'result'
+    end
+
+    it 'call method "open" when call for_item' do
+      @cicindela.should_receive(:open).
+        with("http://localhost/cicindela/recommend?item_id=1&op=for_item&set=test").and_return(StringIO.new('result'))
+      result = @cicindela.for_item(:set => 'test', :item_id => 1)
+      result.should == 'result'
+    end
+  end
+end
+
